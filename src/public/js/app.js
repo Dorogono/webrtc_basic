ee34b356906bb72adf91a49ce37bfb10f0afce24
CHOIJUNHYUK <hyuk.choi@petpeotalk.com>
2023-02-14 23:13:31 +0900

    feat: 비디오 및 오디오 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,86 +1,68 @@
-// 자동으로 찾을 것임
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("#roomName");
-const nameForm = welcome.querySelector("#name");
-const room = document.getElementById("room");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const videoBtn = document.getElementById("video");
+const cameraSelect = document.getElementById("cameras");
 
-room.hidden = true;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-let roomName;
-
-const addMsg = (msg) => {
-  const ul = room.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = msg;
-  ul.appendChild(li);
-};
-
-const handleMsgSubmit = (e) => {
-  e.preventDefault();
-  const input = room.querySelector("#msg input");
-  const value = input.value;
-  socket.emit("new_msg", value, roomName, () => {
-    addMsg(`You: ${value}`);
-  });
-  input.value = "";
-};
-
-const handleNicknameSubmit = (e) => {
-  e.preventDefault();
-  const input = welcome.querySelector("#name input");
-  socket.emit("nickname", input.value);
-};
-
-const showRoom = () => {
-  // welcome.hidden = true;
-  room.hidden = false;
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room: ${roomName}`;
-  const msgForm = room.querySelector("#msg");
-
-  msgForm.addEventListener("submit", handleMsgSubmit);
-};
-
-const handleRoomSubmit = (e) => {
-  e.preventDefault();
-  const input = form.querySelector("input");
-
-  // Custom Event : 인자는 JSON Objet, Call back fn 가능
-  // 단, call back fn은 마지막 값임 (서버 -> 클라이언트로 다 됐다고 알림)
-  socket.emit("enter_room", input.value, showRoom);
-  roomName = input.value;
-  input.value = "";
-};
-
-form.addEventListener("submit", handleRoomSubmit);
-nameForm.addEventListener("submit", handleNicknameSubmit);
-
-socket.on("welcome", (user, newCount) => {
-  console.log(newCount);
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMsg(`${user} Joined!`);
-});
-
-socket.on("new_msg", addMsg);
-
-socket.on("room_change", (rooms) => {
-  const roomList = welcome.querySelector("ul");
-  roomList.innerHTML = "";
-  if (rooms.length === 0) {
-    return;
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      cameraSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
   }
-  rooms.forEach((room) => {
-    const li = document.createElement("li");
-    li.innerText = room;
-    roomList.append(li);
-  });
-});
+};
 
-socket.on("bye", (user, newCount) => {
-  const h3 = room.querySelector("h3");
-  h3.innerText = `Room ${roomName} (${newCount})`;
-  addMsg(`${user} says Good Bye`);
-});
+const getMedia = async () => {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    myFace.srcObject = myStream;
+    await getCameras();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+getMedia();
+
+const handleMuteClick = () => {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (!muted) {
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  } else {
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+};
+const handleVideoClick = () => {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
+  if (cameraOff) {
+    videoBtn.innerText = "Cam Off";
+    cameraOff = false;
+  } else {
+    videoBtn.innerText = "Cam On";
+    cameraOff = true;
+  }
+};
+
+muteBtn.addEventListener("click", handleMuteClick);
+videoBtn.addEventListener("click", handleVideoClick);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import express from "express";
-import { Server } from "socket.io";
-import { instrument } from "@socket.io/admin-ui";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -16,69 +15,6 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`SERVER IS ON: http://localhost:3000`);
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    Credential: true,
-  },
-});
-
-instrument(io, {
-  auth: false,
-});
-
-const publicRooms = () => {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io;
-  const publicRooms = [];
-
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-};
-
-const countRoom = (roomName) => {
-  return io.sockets.adapter.rooms.get(roomName)?.size;
-};
-
-io.on("connection", (socket) => {
-  socket["nickname"] = "Anonymous";
-  // 모든 socket Event 감지
-  socket.onAny((event) => {
-    console.log(`Socket Event: ${event}`);
-  });
-
-  socket.on("enter_room", (roomName, done) => {
-    // 유저는 자기 ID로 된 방에 이미 들어가 있음
-    // 특정 방으로 들어가기
-    socket.join(roomName);
-    done();
-    // 본인 제외 나머지 사람들에게 보냄
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
-    io.sockets.emit("room_change", publicRooms());
-  });
-
-  socket.on("new_msg", (msg, roomName, done) => {
-    socket.to(roomName).emit("new_msg", `${socket.nickname}: ${msg}`);
-    done();
-  });
-
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => {
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1);
-    });
-  });
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms());
-  });
-});
+const io = SocketIO(server);
 
 server.listen(3000, handleListen);
